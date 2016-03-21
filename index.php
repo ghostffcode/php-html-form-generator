a9c5ba5375c191ac185cf098dce414601866ff20
Poetic Bliss <meremezeukah@gmail.com>
2016-03-21 16:27:45 -0400

    Updated the index.php file with comments

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 	require 'form.php';
 
+	// Create an instance of the class
+	// In the following order
+	// ('URL to submit form data', 'post or get', 'name of form')
 	$form = new form('form_process.php', 'post', 'bliss');
 
+	// Create a form element
 	$form->cfElement('select', 'contact', 'col-md-12', ['Male','Female','Other']);
 
 ?>
